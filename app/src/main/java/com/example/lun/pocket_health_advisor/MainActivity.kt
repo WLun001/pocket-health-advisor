ad83fbb3d8d222ef6a4acd2374f8bcf097e163b3
wanching0730 <wanching_97@hotmail.com>
2017-12-21 16:06:57 +0800

    failed onClick for gridview

--- app/src/main/java/com/example/lun/pocket_health_advisor/MainActivity.kt
+++ app/src/main/java/com/example/lun/pocket_health_advisor/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.CardView
 import android.support.v7.widget.Toolbar
 import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.GridLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +25,28 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action kotlin", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        val GridLayout = findViewById(R.id.mainGrid) as GridLayout
+
+        setSingleEvent(gridLayout = GridLayout)
+    }
+
+    fun setSingleEvent(gridLayout: GridLayout){
+        for (count in 0..4){
+            var cardView = gridLayout.getChildAt(count) as CardView
+            val finalI: Int = count
+            cardView.setOnClickListener(object: View.OnClickListener {
+                override fun onClick(v: View?) {
+                    if (count == 3){
+                        val intent = Intent(applicationContext, SinchLoginActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+
+            })
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/example/lun/pocket_health_advisor/SinchLoginActivity.kt
@@ -0,0 +1,16 @@
+package com.example.lun.pocket_health_advisor
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_sinch_login.*
+
+class SinchLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sinch_login)
+    }
+
+}
