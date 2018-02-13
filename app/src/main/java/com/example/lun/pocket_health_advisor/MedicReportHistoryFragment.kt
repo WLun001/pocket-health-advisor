fd2934748294673b60ddf619b893aa6fc4463917
Wei Lun <weilun_95@hotmail.com>
2018-02-13 08:46:48 +0800

    added acuteness for format string

--- app/src/main/java/com/example/lun/pocket_health_advisor/MedicReportHistoryFragment.kt
+++ app/src/main/java/com/example/lun/pocket_health_advisor/MedicReportHistoryFragment.kt
@@ -22,7 +22,7 @@ import org.jetbrains.anko.toast
  */
 class MedicReportHistoryFragment : ListFragment() {
     private var medicReportList = ArrayList<MedicReport>()
-    val mapList = ArrayList<HashMap<*,*>>()
+    val mapList = ArrayList<HashMap<*, *>>()
     private var displayName: String? = ""
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -45,11 +45,11 @@ class MedicReportHistoryFragment : ListFragment() {
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         val id = item?.itemId
-        when(id){
+        when (id) {
             R.id.send_to_hospital -> {
                 val db = FirebaseFirestore.getInstance()
                         .collection("patients")
-                        .whereEqualTo("name",displayName)
+                        .whereEqualTo("name", displayName)
                         .get()
                         .addOnCompleteListener { task ->
                             if (task.isSuccessful) {
@@ -137,23 +137,24 @@ class MedicReportHistoryFragment : ListFragment() {
         return possibleConditionsList
     }
 
-    private fun readDiagnoseCondition(doc: DocumentSnapshot) : Condition{
+    private fun readDiagnoseCondition(doc: DocumentSnapshot): Condition {
         val diagnoseCondition = doc["diagnose_condition"] as HashMap<*, *>
         val name = diagnoseCondition["name"].toString()
         val commonName = diagnoseCondition["common_name"].toString()
-        val acuteness = diagnoseCondition["acuteness"].toString()
+        var acuteness = diagnoseCondition["acuteness"].toString()
         val categories = diagnoseCondition["categories"] as ArrayList<*>
         val hints = (diagnoseCondition["extras"] as HashMap<*, *>)["hint"].toString()
         var prevalence = diagnoseCondition["prevalence"].toString()
         var severity = diagnoseCondition["severity"].toString()
         var triageLevel = diagnoseCondition["triage_level"].toString()
 
+        acuteness = formatString(acuteness)
         prevalence = formatString(prevalence)
         severity = formatString(severity)
         triageLevel = formatString(triageLevel)
 
         val categoriesList = ArrayList<String>()
-        for (cat in categories){
+        for (cat in categories) {
             categoriesList.add(cat.toString())
         }
         return Condition(
@@ -172,7 +173,7 @@ class MedicReportHistoryFragment : ListFragment() {
     private fun readQuestions(doc: DocumentSnapshot): ArrayList<Question> {
         val questionList = ArrayList<Question>()
         val question = doc["questions"] as ArrayList<*>
-        for (i in question){
+        for (i in question) {
             questionList.add(
                     Question(
                             (i as HashMap<*, *>)["question"].toString(),
@@ -185,16 +186,12 @@ class MedicReportHistoryFragment : ListFragment() {
         return questionList
     }
 
-    private fun formatString(s: String) : String{
+    private fun formatString(s: String): String {
         return s.replace('_', ' ')
     }
 
-    private fun setUpListFragment(medicReport: ArrayList<MedicReport>){
+    private fun setUpListFragment(medicReport: ArrayList<MedicReport>) {
         val adapter = MedicReportAdapter(context, medicReport)
         listAdapter = adapter
-    }
-
-
-    private fun objectToMap(obj: Object){
     }
 }
