200ee47c1d90a2d371c61c2041eef0b81aa6e4fe
Wei Lun <weilun_95@hotmail.com>
2018-02-16 14:32:08 +0800

    :remove fab from nearbyhospital activity

--- app/src/main/java/com/example/lun/pocket_health_advisor/NearbyHospitalActivity.kt
+++ app/src/main/java/com/example/lun/pocket_health_advisor/NearbyHospitalActivity.kt
@@ -58,13 +58,9 @@ class NearbyHospitalActivity : AppCompatActivity() {
             if (networkInfo.isConnected)
                 toast("connected")
         }
-        networkInfo ?: kotlin.run { toast("not connected") }
-        getNearbyHospital()
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        networkInfo?: kotlin.run { toast("not connected") }
+        getNearbyHospital()
     }
 
     private fun getNearbyHospital() {
